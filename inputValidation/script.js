5ff68481cb17975b0283fd1871af2275bd48fc93
Lukas <hello@onelifecreative.com>
2015-02-10 20:42:30 +0100

    Create script.js

---
+++ inputValidation/script.js
@@ -0,0 +1,39 @@
+(function(window, document){
+
+	var obj = function() {
+
+		var $ = document,
+			validItem = $.addEventListener("click",function(e) {
+
+			if (e.srcElement.localName === 'input') {
+				obj._init(e);
+			} else {
+				return false;
+			}
+		
+		},false);
+		
+		return {
+
+			_init : function(e){
+					e.target.onblur = this._isNotEmpty;		
+			},
+
+			_isNotEmpty: function(e){
+				var el = e.target;
+				
+				if (el.value === "") {
+					$.getElementById("i"+ el.id).innerHTML = "This value is empty!";
+					el.className = "blad";
+				} else {
+				   $.getElementById("i"+ el.id).innerHTML = "";
+				   el.className = "";
+				}
+			}
+		};
+		
+	}();
+
+	window.valid = obj;
+
+}(window, document));
