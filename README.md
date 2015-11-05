93def6c3f4ad78d5b5cca506e822300a776d82e5
Łukasz Schab <lukasz@onelife.co>
2015-11-05 12:58:35 +0100

    store function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # juggle_js
 My reflections on the JS. A simple script for each day.
+
+
+- Very Simple Cache {{ Object }} Method
+- Input Validation
--- cache_fn/cache.js
+++ cache_fn/cache.js
@@ -1,11 +1,18 @@
-var storeFn = {
 
+/**
+  * param {Object} storeFn
+  * method storeFn.add
+  * if @param { function.name } exist return 
+  */
+
+var storeFn = {
     cacheFn  : {},
         add  : function(fn){     
 	        if (fn.name && !storeFn.cacheFn[fn.name]) {
 	 			storeFn.cacheFn[fn.name] = fn;	
 	        } else {
-	        	console.log( "Add name to fn()!! || The fn[name]() is exist!");
+	        	console.info( "Add name to fn()!! || The fn[name]() is exist!");
+	        	return alert("The fn[name]() is exist!");
 	        }
     }
 };
